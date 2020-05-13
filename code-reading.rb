fb16c04cddbcd1a149c348139ace9ca18b22ac3a
makoto tsuta <makoto1850@gmail.com>
2020-05-14 07:33:22 +0900

    課題追加

---
+++ code-reading.rb
@@ -0,0 +1,24 @@
+（A）8
+（B）10
+（C）13
+（D）11
+（E）15
+（F）4
+（G）6
+（H）12
+（I）9
+（J）14
+（K）3
+（L）2
+（M）5
+（N）7
+（O）17
+（P）1
+（Q）16
+
+① empty_seat変数          空席の数
+② seats_count変数         総座席数
+③ number_of_visitors変数  来店グループ数
+④ users変数               来店人数
+⑤ seating_number変数      来店したグループの着席開始座席番号
+⑥ fill_last_number変数    来店したグループが着席している最後の席番号
